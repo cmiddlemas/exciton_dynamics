6988b4b451a62d3cd9f2a49528ed708aa1c93726
Claire Middlemas <clairemiddlemas@princeton.edu>
2024-04-02 19:49:03 -0400

    Added basic graphdyn module and example

---
+++ analyze_scripts/test_script.py
@@ -0,0 +1,65 @@
+import sys
+# https://stackoverflow.com/questions/5180215/importing-from-subdirectories-in-python
+sys.path.append('../src')
+from graphdyn import *
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib
+import qutip as qt
+import networkx as nx
+import scipy as sp
+
+if __name__ == '__main__':
+    plt.rcParams['text.usetex'] = True
+    # https://stackoverflow.com/questions/3899980/how-to-change-the-font-size-on-a-matplotlib-plot
+    plt.rcParams.update({'font.size': 16})
+
+    leak = 0.3 # Base rate of application of lindblad operator
+    N = 20 # Number of network nodes
+    J = 0.02 # Base coherent coupling constant
+    sigma = 0.02 # Base disorder strength
+    sigmaMod = [2.0, 1.5, 1.0, 0.5, 0.25] # sweep over disorder strength
+    nSamp = 50 # Number of samples in the ensemble
+    hamStore = [] # tell the notebook to keep around the list of computed eigenvalues for some post-processing
+    lioStore = [] # ham = hamiltonian, lio = liouvillian
+
+    graph = nx.star_graph(N-1)
+    C = make_coupling_H(graph)
+    nx.draw(graph)
+
+    Leig = []
+    Heig = []
+    for mod in sigmaMod:
+        print(mod)
+        Lacc = []
+        Hacc = []
+        for i in range(nSamp):
+            H = diagonal_H(N) + diagonal_disorder(N, mod*sigma) + J*C
+            L = qt.liouvillian(H, leak*collective_lowering(N))
+            NSpace = sp.linalg.null_space(L.full())
+            print(np.shape(NSpace)) # Check whether we have unique zero eigenvector (i.e. choice of stable base)
+            Lacc += list(L.eigenenergies())
+            Hacc += list(H.eigenenergies())
+        Leig += [Lacc]
+        Heig += [Hacc]
+    lioStore += [Leig]
+    hamStore += [Heig]
+    # https://matplotlib.org/stable/gallery/lines_bars_and_markers/errorbar_subsample.html#sphx-glr-gallery-lines-bars-and-markers-errorbar-subsample-py
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize= [6, 9])
+
+    ax1.hist(Heig, 100,density=True, histtype='step',
+             label=["$\sigma/J =$ 2.0", "$\sigma/J =$ 1.5", "$\sigma/J =$ 1.0",
+                    "$\sigma/J =$ 0.5", "$\sigma/J =$ 0.25"])
+    ax1.set_title("Hamiltonian Spectrum")
+    ax1.set_xlabel(r'$E$')
+    ax1.set_ylabel(r'$\sigma(E)$')
+    ax1.legend(loc = 'upper left')
+
+    for l in Leig:
+        ax2.scatter(np.real(l), np.imag(l), s=2)
+    ax2.set_title("Liouvillian Spectrum")
+    ax2.set_xlabel(r'Re$(\lambda)$')
+    ax2.set_ylabel(r'Im$(\lambda)$')
+
+    plt.tight_layout()
+    plt.savefig('test_script.png')
---
+++ src/graphdyn.py
@@ -0,0 +1,98 @@
+"""
+graphdyn.py
+
+Allows a user to take a NetworkX graph and convert it to a QuTiP superoperator
+in the single-excitation + ground-state subspace. The convention is that the
+excited states occupy the first n dimensions of the underlying ket space and the
+ground state occupies the last.
+
+Also provides helper functions for setting up common analyses such as tracking
+eigenvalues movement as afunction of diagonal disorder and setting up common
+plots, which can then be edited in the calling notebook or script.
+"""
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib as mpl
+import qutip as qt
+import networkx as nx
+import scipy as sp
+
+
+
+def lowering_L(n, i):
+    """Creates a lowering operator on a network of n nodes at site i"""
+    M = np.zeros((n+1, n+1))
+    M[-1,i] = 1.0
+    return qt.Qobj(M)
+
+def dephasing_L(n, i):
+    """Creates a dephasing operator on a network of n nodes at site i"""
+    M = np.zeros((n+1, n+1))
+    M[i,i] = 1.0
+    return qt.Qobj(M)
+
+def collective_lowering(n):
+    """
+    Creates a collective (i.e. acting symmetrically on all sites) lowering
+    operator on a network of n nodes
+    """
+    return sum([lowering_L(n, i) for i in range(n)])/np.sqrt(n)
+
+def collective_raising(n):
+    """
+    Creates a collective (i.e. acting symmetrically on all sites) raising
+    operator on a network of n nodes
+    """
+    return sum([lowering_L(n, i).dag() for i in range(n)])/np.sqrt(n)
+
+def independent_lowering(n, sqrt_rate):
+    """
+    Creates a list of independent, identical lowering operators on a network of
+    n nodes with coefficient sqrt_rate
+    """
+    return [sqrt_rate*lowering_L(n, i) for i in range(n)]
+
+def independent_dephasing(n, sqrt_rate):
+    """
+    Creates a list of independent, identical dephasing operators on a network of
+    n nodes with coefficient sqrt_rate
+    """
+    return [sqrt_rate*dephasing_L(n, i) for i in range(n)]
+
+def make_vac(array):
+    """
+    Takes an array representing a nxn matrix representing the Hamiltonian of the
+    excited states and returns a (n+1)x(n+1) matrix representing the addition of
+    the ground state.
+    """
+    n = array.shape[0]
+    temp = np.concatenate((array, np.zeros((1, n))))
+    return np.concatenate((temp, np.zeros((n+1, 1))), axis=1)
+
+def diagonal_H(n):
+    """
+    Returns a Hamiltonian that is an identity on the subspace of n excited
+    states and zero on the ground state.
+    """
+    M = np.identity(n+1)
+    M[-1,-1] = 0.0
+    return qt.Qobj(M)
+
+def make_coupling_H(G):
+    """
+    Takes a NetworkX graph G and returns the corresponding QuTiP Hamiltonian in
+    the single-excitation + ground-state subspace.
+    """
+    M = nx.to_numpy_array(G)
+    return qt.Qobj(make_vac(M))
+
+def diagonal_disorder(n, sigma, rng=np.random.default_rng()):
+    """
+    Returns a Hamiltonian that is diagonal with entries normally distributed
+    with standard deviation sigma on the subspace of n excited states and zero
+    on the ground state. Also allows you to specify an instantiation of an rng,
+    provided this rng provides rng.normal(offset, sigma, size). 
+    """
+    diag = rng.normal(0.0, sigma, n+1)
+    diag[-1] = 0.0
+    return(qt.Qobj(np.diag(diag)))
